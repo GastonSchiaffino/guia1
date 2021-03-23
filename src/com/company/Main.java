84d7133931724e86742681f944381444342c3bc7
Gaston <gastonschiaffino10@gmail.com>
2021-03-23 00:18:51 -0300

    guia1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,13 @@
 package com.company;
 
 import com.company.ejercicio1.rectangle;
+import com.company.ejercicio2.employee;
 
 public class Main {
 
     public static void main(String[] args) {
 
+        /// Ejercicio 1
         rectangle rectangle = new rectangle();
         System.out.println("Ancho: " +rectangle.getBroad());
         System.out.println("Altura: " +rectangle.getHeight());
@@ -16,6 +18,30 @@ public class Main {
         rectangle.setHeight(7);
         System.out.println("Area: " +rectangle.calculateArea());
         System.out.println("Perimetro: " +rectangle.calculatePerimeter());
+
+        ///Ejercicio 2
+        ///Inciso A
+        employee employee1 = new employee();
+        employee employee2 = new employee();
+
+        employee1.setName("Carlos");
+        employee1.setSurname("Gutierrez");
+        employee1.setDni("23456345");
+        employee1.setSalary(25000);
+
+        ///Inciso B
+        employee2.setName("Ana");
+        employee2.setSurname("Sanchez");
+        employee2.setDni("34234123");
+        employee2.setSalary(27500);
+
+        ///Inciso C
+        System.out.println( employee1.getEmployee());
+        System.out.println( employee2.getEmployee());
+
+        ///Inciso D
+        employee1.increaseSalary(15);
+        System.out.println("Salario Anual Aumentado: " + employee1.calculateAnnualSalary());
     }
 
 }
