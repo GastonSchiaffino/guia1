8407332e123c6ed2b1dc79b7651beed1494a2626
Gaston <gastonschiaffino10@gmail.com>
2021-03-22 23:43:07 -0300

    guia1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,21 @@
 package com.company;
 
+import com.company.ejercicio1.rectangle;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        rectangle rectangle = new rectangle();
+        System.out.println("Ancho: " +rectangle.getBroad());
+        System.out.println("Altura: " +rectangle.getHeight());
+        System.out.println("Area: " +rectangle.calculateArea());
+        System.out.println("Perimetro: " +rectangle.calculatePerimeter());
+
+        rectangle.setBroad(5);
+        rectangle.setHeight(7);
+        System.out.println("Area: " +rectangle.calculateArea());
+        System.out.println("Perimetro: " +rectangle.calculatePerimeter());
     }
+
 }
--- src/com/company/ejercicio1/rectangle.java
+++ src/com/company/ejercicio1/rectangle.java
@@ -1,4 +1,38 @@
 package com.company.ejercicio1;
 
 public class rectangle {
+    private double broad = 1.0;
+    private double height = 1.0;
+
+    public rectangle(){
+    }
+
+    public  rectangle(double broad, double height){
+        this.broad = broad;
+        this.height = height;
+    }
+
+    public double getBroad() {
+        return broad;
+    }
+
+    public void setBroad(double broad) {
+        this.broad = broad;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double calculateArea(){
+        return broad * height;
+    }
+
+    public double calculatePerimeter(){
+        return 2 * (broad + height);
+    }
 }
